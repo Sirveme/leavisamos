a3c3d018ab9684373209a47113d885a6a2f0d266
Mesas <asesorautorizado.duilio@gmail.com>
2026-01-07 19:05:37 -0500

    fy
    j

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import FileResponse
 from .database import engine, Base
-from .routers import auth, dashboard, ws
+from .routers import auth, dashboard, ws, api
 from .config import THEMES
 
 # Base.metadata.create_all(bind=engine) # Descomentar solo si usas SQLite local
@@ -33,6 +33,7 @@ async def add_domain_context(request: Request, call_next):
 app.include_router(auth.router)
 app.include_router(dashboard.router)
 app.include_router(ws.router)
+app.include_router(api.router)
 
 @app.get("/service-worker.js")
 async def get_service_worker():
